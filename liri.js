1e8ac9132957209e4b238035df67fe05260ebbe7
Claire Stolp <clairemstolp@gmail.com>
2018-06-23 18:06:01 -0400

    added inquirer

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ const twitter = require('twitter');
 const nodeSpotifyApi = require('node-spotify-api');
 const request = require('request');
 const dotenv = require('dotenv').config();
+const inquirer = require('inquirer');
 
-const spotify = new spotify(keys.spotify);
-var client = new twitter(keys.twitter);
+const spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
